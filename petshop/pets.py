b24aba3c28028f09cb66f6480b22aaaa2529a7c8
AravindIM <iamaravindim@gmail.com>
2021-07-17 21:34:10 +0530

    Bug Fixes

--- petshop/pets.py
+++ petshop/pets.py
@@ -100,7 +100,7 @@ def edit(pid):
     elif request.method == "POST":
         description = request.form.get('description')
         sold = request.form.get("sold")
-        if sold == "true":
+        if sold == "1":
             cursor.execute("update pet set sold = DATE() where id = ?", [pid])
         cursor.execute("update pet set description = ? where id = ?", [description, pid])
         conn.commit()
